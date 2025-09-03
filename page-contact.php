409005c6d3f8704f51b4169ad9c7a703020b9666
farazhani <hanifaraz82@gmail.com>
2025-09-03 17:44:50 +0330

    Update page-contact.php
    صفحه ارتباط با ما تموم شد

--- page-contact.php
+++ page-contact.php
@@ -1,13 +1,53 @@
-  <?php get_header() ?>
-    <main class="mx-auto max-w-120 bg-white rounded-xl my-20 p-5">
-      <?php
-      the_content();
-      ?>
-    </main>
-  <?php get_footer()
-  ?>
+<?php
+// get_header(); ?>
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+<br/>
+<div class="items-center justify-center h-screen bg-gray-200 shadow-md rounded-lg p-6 <br/>">
+<h3 class="text-center">
+ما یک سوپرمارکت اشتراکی هستیم که با همراهی اعضای محله شکل گرفته‌ایم تا خرید روزمره رو ساده‌تر، منصفانه‌تر و دل‌چسب‌تر کنیم
+</h3>
+<h3 class="text-center">
+ باور داریم که همکاری، اعتماد و ارتباط نزدیک با مشتری‌ها، قلب تپنده‌ی این فروشگاهه
+</h3>
+<h3 class="text-center">
+ اگر سوالی داری، پیشنهادی داری یا فقط می‌خوای سلامی بدی، خوشحال می‌شیم صدات رو بشنویم
+</h3>
+<h3 class="text-center">
+ راه‌های ارتباطی پایین همین صفحه منتظر تو هستن. با هم، بهتر می‌سازیم
+</h3>
+<br/>
+<br/>
+<br/>
+<br/>
+<p class="text-sm font-medium text-center">پشتیبانی:09039036722</p>
+
+ 
+    <div  class="justify-center">
+      <label class=" text-center block text-sm font-medium text-gray-700">نام و نام خانوادگی</label>
+      <div class="flex justify-center items-center ">
+      <input type="text"  cols="45" name="full_name" required class="  mt-1 block  rounded-md border-gray-800 shadow-md focus:ring-green-500 focus:border-green-500">
+    </div>
+    </div>
+    <div  class="justify-center">
+      <label class=" text-center block text-sm font-medium text-gray-700"> ایمیل</label>
+      <div class="flex justify-center items-center ">
+      <input type="email" name="full_name"  cols="45" required class="  mt-1 block  rounded-md border-gray-800 shadow-md focus:ring-green-500 focus:border-green-500">
+    </div>
+    </div>
+
+    <div class="justify-center">
+      <label class=" text-center block text-sm font-medium text-gray-700">دریافت پیام</label>
+      <div class="flex justify-center items-center ">
+      <textarea name="message" rows="5" cols="45" required class=" mt-1 block rounded-md border-gray-300 shadow-sm focus:ring-green-500 focus:border-green-500"></textarea>
+    </div>
+    </div>
+     <div class=" justify-center items-center ">
+    <button type="submit" class="text-center w-50 bg-green-400 text-white py-2 px-4 rounded-md hover:bg-green-700 transition">ارسال پیام</button>
+ </div>
+</div>
+
+<!-- <?php get_footer(); ?> -->
 
   
-  
-  
+
 
